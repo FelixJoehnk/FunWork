45580856f449272bb2c75fddd218ff79f92539cd
FelixJoehnk <felixjoehnk@gmx.de>
2020-10-23 23:21:50 +0200

    collision with ball

--- Unity/Transmission/Assets/Transmission/Scenes/Felix/PlayerMovement.cs
+++ Unity/Transmission/Assets/Transmission/Scenes/Felix/PlayerMovement.cs
@@ -2,23 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour{
 
     private Vector3 mouse_pos_old;
     private float speed;
 
     
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         mouse_pos_old = Input.mousePosition;
         speed = .5f;
     }
-
+    
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         //transform.position += Vector3.forward;
         
 
@@ -30,11 +27,15 @@ public class PlayerMovement : MonoBehaviour
 
         transform.position +=  speed * transform.forward * Time.deltaTime;
 
-        if(Input.GetKey(KeyCode.Mouse0)){
-            speed *=1.1f;
-        }else if(Input.GetKey(KeyCode.Mouse1)){
-            speed /=1.1f;
-        }
+        if(Input.GetKey(KeyCode.Mouse0))speed *=1.1f;
+        if(Input.GetKey(KeyCode.Mouse1))speed /=1.1f;
+        
         
     }
+
+    void OnCollisionEnter(Collision collision){
+        Debug.Log("In a Collision \n ");
+        print("In a Collision\n ");
+    }
+    
 }
